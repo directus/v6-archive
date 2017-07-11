883bd2e8bea38fd385e9adae806b9e5f83f7ffdc
Rijk van Zanten <rijkvanzanten@me.com>
2017-07-11 23:40:50 +0200

    Add logic for saving custom value radio

--- app/core/interfaces/radio_buttons/interface.js
+++ app/core/interfaces/radio_buttons/interface.js
@@ -19,14 +19,24 @@ define([
   return UIView.extend({
     template: 'radio_buttons/input',
     events: {
-      'change input[type=radio]': 'updateValue'
+      'change input[type=radio]': 'updateValue',
+      'focus input[type=text]': 'checkRadio',
+      'input input[type=text]': 'updateCustom'
     },
     updateValue: function (event) {
       this.model.set(this.name, event.currentTarget.value);
     },
+    updateCustom: function (event) {
+      event.target.parentNode.querySelector('input[type=radio]').value = event.target.value;
+      this.model.set(this.name, event.target.value);
+    },
+    checkRadio: function (event) {
+      event.target.parentNode.querySelector('input[type=radio]').checked = true;
+    },
     serialize: function () {
       var value = this.options.value || this.columnSchema.get('default_value') || '';
       var options = parseOptions(this.options.settings.get('options'));
+
       var optionsArray = Object.keys(options).map(function (key) {
         return {
           key: key,
@@ -35,12 +45,35 @@ define([
         };
       });
 
+      var optionKeys = optionsArray.map(function (option) {
+        return option.key;
+      });
+
+      var optionValues = optionsArray.map(function (option) {
+        return option.value;
+      });
+
+      var customOption;
+
+      if (optionKeys.indexOf(value) >= 0 || optionValues.indexOf(value) >= 0) {
+        customOption = {
+          value: '',
+          selected: false
+        };
+      } else {
+        customOption = {
+          value: value,
+          selected: true
+        };
+      }
+
       return {
         options: optionsArray,
         name: this.options.name,
         comment: this.options.schema.get('comment'),
         readOnly: this.options.settings.get('read_only') || !this.options.canWrite,
         allowOther: this.options.settings.get('allow_other'),
+        customOption: customOption,
         value: value
       };
     }
