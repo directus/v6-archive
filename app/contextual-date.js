36c0687f7333e546a44daa764e4ac1bff7345dd2
rijkvanzanten <rijkvanzanten@me.com>
2017-11-06 15:29:17 -0500

    Cleanup contextual-date file

--- app/contextual-date.js
+++ app/contextual-date.js
@@ -1,26 +1,39 @@
-define(['moment', 'underscore', 'jquery', 'core/interval'], function (moment, _, $, interval) {
+/**
+ * This logic is used to update the content of elements with the contextual-date-updater
+ *   class every 60 seconds, to keep the relational timestamp up to date
+ *
+ * /app/templates/modules/activity/activity-history.handlebars
+ *
+ * TODO: Move this functionality to the View that renders this element. Should
+ *   only star the interval and update cycle when the element is actually rendered
+ *   and clear the interval when it's not needed anymore
+ */
+
+define(function (require) {
   'use strict';
 
-  // time in milliseconds before update
+  var moment = require('moment');
+  var _ = require('underscore');
+  var $ = require('jquery');
+  var interval = require('core/interval');
+
+  // Interval time every n milliseconds
+  // TODO: Only has to update every minute up to an hour. After an hour, it only
+  //   has to update hourly, etc
   var waitTime = 60000;
-  // loop through all the contextual date updater
-  // every 60s and update its contextual date
-  var updateTime = function () {
+
+  function updateTime() {
     var $elements = $('.contextual-date-updater[data-date]');
 
     _.each($elements, function (element) {
       var $element = $(element);
       var date = moment($element.data('date'));
 
-      // @TODO: update gradually
-      // update by minutes until an hour
-      // update by hour until 24
-      // update by date? maybe is too much
       if (date.isValid()) {
         $element.text(date.fromNow());
       }
     });
-  };
+  }
 
   interval(updateTime, waitTime);
 });
